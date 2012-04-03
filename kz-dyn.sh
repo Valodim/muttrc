c5539fcf6633781e5b7d211d05f20701330a3409
Valodim <valodim@mugenguild.com>
2012-04-03 04:56:28 +0200

    limit regular queries to 200

--- kz-dyn.sh
+++ kz-dyn.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 for i in $(python -c 'import notmuch; print notmuch.Database().get_all_tags();'); do
-    echo "virtual-mailboxes \"> $i\" \"notmuch://\$HOME/Maildir?query=tag:$i\""
+    echo "virtual-mailboxes \"> $i\" \"notmuch://\$HOME/Maildir?limit=200&query=tag:$i\""
 done
