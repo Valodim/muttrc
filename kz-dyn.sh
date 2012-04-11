991aa150cdd71344978762ace24ea7f81fa1fb05
Valodim <valodim@mugenguild.com>
2012-04-11 14:22:32 +0200

    add "recent" virtual mailbox

--- kz-dyn.sh
+++ kz-dyn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "virtual-mailboxes \"recent\" \"notmuch://\$HOME/Maildir?query=$(date +%s -d yesterday)..$(date +%s)\""
+
 for i in $(python -c 'import notmuch; print notmuch.Database().get_all_tags();'); do
     echo "virtual-mailboxes \"> $i\" \"notmuch://\$HOME/Maildir?limit=200&query=tag:$i\""
 done
