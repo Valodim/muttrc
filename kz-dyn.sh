b91a1942893be4b47f2a954792ccc9fe455064a5
Valodim <valodim@mugenguild.com>
2012-04-11 14:22:06 +0200

    use bash instead of zsh for kz-dyn

--- kz-dyn.sh
+++ kz-dyn.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 for i in $(python -c 'import notmuch; print notmuch.Database().get_all_tags();'); do
     echo "virtual-mailboxes \"> $i\" \"notmuch://\$HOME/Maildir?limit=200&query=tag:$i\""
